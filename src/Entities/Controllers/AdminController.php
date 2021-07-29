d7fecc769332bbdf04ae08d552b2f1c877a7a7d8
Juan Villalta <juanfv2@gmail.com>
2021-07-29 13:41:48 -0600

    admin:sentry-context

---
+++ src/Entities/Controllers/AdminController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\File;
+
+class AdminController extends Controller
+{
+    public function admin()
+    {
+        \Debugbar::disable();
+
+        $index = public_path('_.admin/index.html');
+
+        if (File::exists($index)) {
+            return File::get($index);
+        }
+
+        return view('under-construction');
+    }
+
+    public function adminDevelopment()
+    {
+        \Debugbar::enable();
+
+        $index = public_path('_.admin/index.html');
+
+        if (File::exists($index)) {
+            return File::get($index);
+        }
+
+        return $this->admin();
+    }
+
+    public function artisan($key1, $key2 = null)
+    {
+
+        // ? artisan:
+        $k = $key1 . ($key2 ? ':' . $key2 : '');
+
+        try {
+            // echo '<br>php artisan view:clear...';
+            $parameters = request('params', '[]');
+
+            $params = json_decode($parameters, true);
+
+            \Illuminate\Support\Facades\Artisan::call($k, $params);
+
+            $params['cmd'] = '<br>php artisan ' . $k . ' ' . $parameters;
+            // $params['msg'] = '<br>php artisan ' . $k . ' completed';
+            return $params;
+        } catch (\Exception $e) {
+            // echo '<br>' . $e->getMessage();
+            return $e;
+        }
+    }
+}
---
+++ src/Middleware/SentryContext.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Juanfv2\BaseCms\Middleware;
+
+use Closure;
+use Sentry\State\Scope;
+
+class SentryContext
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure                 $next
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (app()->bound('sentry')) {
+            // \Sentry\configureScope(function (Scope $scope): void {
+            //     $c = request()->headers->get('r-country', '--');
+            //     $scope->setTag('r-country', $c);
+            // });
+            if (auth()->check()) {
+                \Sentry\configureScope(function (Scope $scope): void {
+                    $scope->setUser([
+                        'id' => auth()->user()->id,
+                        'email' => auth()->user()->email,
+                    ]);
+                });
+            }
+        }
+
+        return $next($request);
+    }
+}
