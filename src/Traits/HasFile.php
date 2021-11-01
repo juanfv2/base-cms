e0a796f7b051ae53821f70b5476fdb8cc9c53e03
Juan Villalta <juanfv2@gmail.com>
2021-11-01 12:04:57 -0600

    HasFile

---
+++ src/Traits/HasFile.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace  Juanfv2\BaseCms\Traits;
+
+trait HasFile
+{
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function hasOneXFile($field)
+    {
+        return $this->hasOne(\App\Models\Auth\XFile::class, 'entity_id')
+            ->where('entity', $this->table)
+            ->where('field', $field)->withDefault([
+                'id' => 1,
+                'name' => 'logo.png',
+                'field' => $field,
+                'entity' => $this->table,
+                'extension' => 'png',
+                'publicPath' => '/storage/assets/images/logo.png',
+            ]);
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function hasManyXFile($field)
+    {
+        return $this->hasMany(\App\Models\Auth\XFile::class, 'entity_id')
+            ->where('entity', $this->table)
+            ->where('field', $field);
+    }
+}
