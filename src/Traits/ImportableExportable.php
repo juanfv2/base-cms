8048c7a63ed6f3e77e5ec057d2ad61b7579995fb
Juan Villalta <juanfv2@gmail.com>
2023-08-10 16:35:15 -0600

    fix: import csv

--- src/Traits/ImportableExportable.php
+++ src/Traits/ImportableExportable.php
@@ -154,8 +154,20 @@ trait ImportableExportable
 
         foreach ($headers as $k) {
             if (isset($keys[$k])) {
-                if (! empty(trim((string) $data[$k]))) {
-                    $dataToSave[$keys[$k]] = $data[$k];
+                // if (! empty(trim($data[$k]))) {
+                //     $dataToSave[$keys[$k]] = $data[$k];
+                // }
+
+                // if ($data[$k] == '[[v]]') {
+                //     $dataToSave[$keys[$k]] = null;
+                // }
+
+                // logger(__FILE__.':'.__LINE__.' $k, $keys[$k] ', [$k, $keys[$k], $data[$k]]);
+
+                $dataToSave[$keys[$k]] = trim($data[$k]);
+
+                if ($data[$k] === '') {
+                    $dataToSave[$keys[$k]] = null;
                 }
             }
         }
