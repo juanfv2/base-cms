3e8e7e4ce5c127a562afd77b30251fe6ec0e0bf1
Juan Villalta <juanfv2@gmail.com>
2023-10-18 11:23:12 -0600

    createAuthUser, updateAuthUser

--- src/Traits/BaseCmsModelUser.php
+++ src/Traits/BaseCmsModelUser.php
@@ -7,6 +7,7 @@ use App\Models\Auth\Person;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
 
 trait BaseCmsModelUser
 {
@@ -89,19 +90,58 @@ trait BaseCmsModelUser
         });
     }
 
-    public function deleteAuthUser()
+    public function deleteAuthUser($type)
     {
-        return DB::transaction(function () {
+        return DB::transaction(function () use ($type) {
             $this->email = $this->email.'-deleted-'.$this->id.'-'.time();
             $this->save();
-            if ($this->person) {
-                $this->person->delete();
-            }
-            if ($this->account) {
-                $this->account->delete();
-            }
+            $this->$type->delete();
 
             return $this->delete();
+        });
+    }
+
+    public function createAuthUser($input, $type)
+    {
+        return DB::transaction(function () use ($input, $type) {
+            $input['email'] = Str::lower($input['email']);
+            $input['password'] = Hash::make($input['password']);
+            $input['roles'] = is_string($input['roles']) ? json_decode($input['roles'], null, 512, JSON_THROW_ON_ERROR) : $input['roles'];
+            $r = $this->mSave($input);
+
+            $input['id'] = $r->id;
+            $input['user_id'] = $r->id;
+
+            $mType = new $type;
+            $mType->mSave($input);
+
+            if (! isset($input['uid'])) {
+                $r->verifyUser();
+            }
+
+            return $r;
+        });
+    }
+
+    public function updateAuthUser($input, $type)
+    {
+        return DB::transaction(function () use ($input, $type) {
+            Schema::disableForeignKeyConstraints();
+
+            if (isset($input['email'])) {
+                $input['email'] = Str::lower($input['email']);
+            }
+
+            if (isset($input['password'])) {
+                $input['password'] = Hash::make($input['password']);
+            }
+
+            $updated = $this->mSave($input);
+            $updated = $this->$type->mSave($input);
+
+            Schema::enableForeignKeyConstraints();
+
+            return $updated;
         });
     }
 }
