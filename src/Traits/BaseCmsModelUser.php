68bd77314d7d031e3b4ec4ff514e34efd849e4a3
Juan Villalta <juanfv2@gmail.com>
2023-10-22 11:54:10 -0600

    ...

--- src/Traits/BaseCmsModelUser.php
+++ src/Traits/BaseCmsModelUser.php
@@ -11,89 +11,10 @@ use Illuminate\Support\Str;
 
 trait BaseCmsModelUser
 {
-    public function withAdditionalInfo($type, $input)
-    {
-        $entity = "{$input['withEntity']}_{$type}_with";
-
-        return $this->$entity($input);
-    }
-
-    public function auth_people_create_with($input)
-    {
-        return DB::transaction(function () use ($input) {
-            $input['password'] = Hash::make($input['password']);
-            $input['roles'] = is_string($input['roles']) ? json_decode($input['roles'], null, 512, JSON_THROW_ON_ERROR) : $input['roles'];
-            $r = $this->mSave($input);
-
-            $input['id'] = $r->id;
-            $input['user_id'] = $r->id;
-            $person = new Person;
-            $person->mSave($input);
-
-            return $r;
-        });
-    }
-
-    public function auth_people_update_with($input)
-    {
-        return DB::transaction(function () use ($input) {
-            Schema::disableForeignKeyConstraints();
-
-            if (isset($input['password'])) {
-                $input['password'] = Hash::make($input['password']);
-            }
-
-            $updated = $this->mSave($input);
-            $this->person->mSave($input);
-
-            Schema::enableForeignKeyConstraints();
-
-            return true;
-        });
-    }
-
-    public function auth_accounts_create_with($input)
-    {
-        return DB::transaction(function () use ($input) {
-            $input['password'] = Hash::make($input['password']);
-            $input['roles'] = is_string($input['roles']) ? json_decode($input['roles'], null, 512, JSON_THROW_ON_ERROR) : $input['roles'];
-            $r = $this->mSave($input);
-
-            $input['id'] = $r->id;
-            $input['user_id'] = $r->id;
-            $account = new Account;
-            $account->mSave($input);
-
-            if (! isset($input['uid'])) {
-                $r->verifyUser();
-            }
-
-            return $r;
-        });
-    }
-
-    public function auth_accounts_update_with($input)
-    {
-        return DB::transaction(function () use ($input) {
-            Schema::disableForeignKeyConstraints();
-
-            if (isset($input['password'])) {
-                $input['password'] = Hash::make($input['password']);
-            }
-
-            $updated = $this->mSave($input);
-            $this->account->mSave($input);
-
-            Schema::enableForeignKeyConstraints();
-
-            return true;
-        });
-    }
-
     public function deleteAuthUser($type)
     {
         return DB::transaction(function () use ($type) {
-            $this->email = $this->email.'-deleted-'.$this->id.'-'.time();
+            $this->email = $this->email . '-deleted-' . $this->id . '-' . time();
             $this->save();
             $this->$type->delete();
 
@@ -115,7 +36,7 @@ trait BaseCmsModelUser
             $mType = new $type;
             $mType->mSave($input);
 
-            if (! isset($input['uid'])) {
+            if (!isset($input['uid'])) {
                 $r->verifyUser();
             }
 
